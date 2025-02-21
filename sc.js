498346efea02fd492b144f1857c64e5cf70557b4
Suhofruct_155 <msemeletov@inbox.ru>
2025-02-21 16:29:33 +0900

    Добавил файлы

---
+++ sc.js
@@ -0,0 +1,19 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slider img');
+
+function showSlide(index){
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function prevSlide(){
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex)
+}
+
+function nextSlide(){
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex)
+}
+showSlide(currentIndex)
